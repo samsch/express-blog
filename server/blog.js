791c85f680c0aca8e8d2c388b5edc6fc6e66d658
Samuel Scheiderich <samuel.scheiderich@gmail.com>
2017-10-19 14:28:43 -0400

    Functional backend blog application. Minimal features.

---
+++ server/blog.js
@@ -0,0 +1,97 @@
+'use strict';
+const express = require('express');
+
+module.exports.getPublicRouter = knex => {
+  const router = express.Router();
+
+  router.get('/posts', (req, res) => {
+    knex('post').then(posts => {
+      res.json({
+        posts,
+      });
+    });
+  });
+
+  router.get('/post/:id', (req, res) => {
+    knex('post')
+      .where({ id: req.params.id })
+      .then(posts => {
+        if (posts.length === 0) {
+          res.status(404).json({
+            error: 'Post ID not found',
+          });
+        } else if (posts.length !== 1) {
+          res.status(500).json({
+            error:
+              'Something went wrong retrieving this post, please try again',
+          });
+        } else {
+          res.json({
+            post: posts[0],
+          });
+        }
+      });
+  });
+
+  router.get('/authors', (req, res) => {
+    knex('author')
+      .columns(['id', 'name'])
+      .then(authors => {
+        res.json({
+          authors,
+        });
+      });
+  });
+
+  return router;
+};
+
+const blogPostFields = [
+  {
+    field: 'title',
+    valid: value => typeof value === 'string' && value.length > 0,
+  },
+  {
+    field: 'post',
+    valid: value => typeof value === 'string' && value.length > 0,
+  },
+];
+
+module.exports.getPrivateRouter = knex => {
+  const router = express.Router();
+
+  router.post('/post', (req, res) => {
+    const newPost = req.body.post;
+    if (!blogPostFields.every(({ field, valid }) => valid(newPost[field]))) {
+      res.status(400).json({
+        error: 'New post must contain a title and body.',
+      });
+    } else {
+      newPost.author = req.author.id;
+      knex('post')
+        .insert(
+          blogPostFields.reduce(
+            (post, { field }) =>
+              Object.assign(post, { [field]: newPost[field] }),
+            {
+              author: req.author.id,
+            }
+          )
+        )
+        .then(id => {
+          res.json({
+            message: 'Added post',
+            id,
+          });
+        })
+        .catch(error => {
+          console.log('Error adding post', error);
+          res.status(500).json({
+            error: 'Something went wrong, please try again',
+          });
+        });
+    }
+  });
+
+  return router;
+};
--- server/main.js
+++ server/main.js
@@ -9,7 +9,18 @@ const app = express();
 const bodyParser = require('body-parser');
 const Promise = require('bluebird');
 const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 const scrypt = require('scrypt-for-humans');
+const randomNumber = require('random-number-csprng');
+const blogRouters = require('./blog');
+
+var knex = require('knex')({
+  client: 'pg',
+  connection: config.pgconnection,
+  version: '10',
+});
+
+const store = new KnexSessionStore({ knex });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -20,43 +31,81 @@ app.use(
     secure: true,
     resave: false,
     saveUninitialized: false,
+    store,
   })
 );
 
-app.post('/login', function(req, res) {
-  if (!req.body.username || !req.body.password) {
+app.post('/api/login', function(req, res) {
+  if (!req.body.email || !req.body.password) {
     res.json({
-      error: true,
-      message:
-        'Missing request parameter(s). Expected json object with username and password keys.',
+      error:
+        'Missing request parameter(s). Expected json object with email and password keys.',
     });
     return;
   }
   Promise.try(function() {
-    return scrypt.verifyHash(
-      req.body.password,
-      config.users[req.body.username]
-    );
+    return randomNumber(0, 200);
   })
-    .then(function() {
-      req.session.user = req.body.username;
-      res.json({
-        error: false,
-        message: 'Successfully logged in',
-      });
+    .then(random => {
+      return Promise.all([
+        Promise.delay(random),
+        knex('author').where({ email: req.body.email }),
+      ]);
     })
-    .catch(function(error) {
+    .then(([, authors]) => {
+      if (authors.length !== 1) {
+        throw new Error('invalid');
+      }
+      return scrypt
+        .verifyHash(req.body.password, authors[0].password)
+        .then(function() {
+          req.session.authorId = authors[0].id;
+          res.json({
+            message: 'Successfully logged in',
+            author: authors[0],
+          });
+        });
+    })
+    .catch(() => {
       res.json({
-        error: true,
-        message: 'Invalid username+password',
+        error: 'Invalid email or password',
       });
     });
 });
 
-app.get('/', function(req, res) {
-  req.session.viewCount = req.session.viewCount ? req.session.viewCount + 1 : 1;
-  res.send(`Hello World! You have been here ${req.session.viewCount} times.`);
-});
+const authenticatedMiddleware = (req, res, next) => {
+  Promise.try(() => {
+    const authorId = req.session.authorId;
+    if (!authorId) {
+      throw new Error('Unauthenticated');
+    }
+    return knex('author').where({ id: authorId });
+  })
+    .then(authors => {
+      if (authors.length !== 1) {
+        throw new Error('Unauthenticated');
+      }
+      req.author = authors[0];
+      next();
+    })
+    .catch(error => {
+      if (error.message === 'Unauthenticated') {
+        res.status(401).json({
+          error: 'User not authenticated, please login.',
+        });
+      } else {
+        res.status(500).json({
+          error:
+            'Something went wrong processing your request, please try again.',
+        });
+      }
+    });
+};
+
+app.use('/api', blogRouters.getPublicRouter(knex));
+app.use('/api', authenticatedMiddleware, blogRouters.getPrivateRouter(knex));
+
+app.use(express.static('build'));
 
 // If securePort is defined in config, then we want to enable serving
 // over https on that port. Otherwise, the app is probably being served
